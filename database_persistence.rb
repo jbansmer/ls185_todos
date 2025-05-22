3f8d97d8ebd9a4460abba25c30c48d9874bbeaa6
Jacob Bansmer <jbansmer@gmail.com>
2025-05-22 02:19:08 -0700

    new ENV variable

--- database_persistence.rb
+++ database_persistence.rb
@@ -4,7 +4,7 @@ require "pg"
 class DatabasePersistence
   def initialize(logger)
     @db = if Sinatra::Base.production?
-            PG.connect(ENV['DATABASE_URL'])
+            PG.connect(ENV['HEROKU_POSTGRESQL_GOLD_URL'])
           else
             PG.connect(dbname: "todos")
           end
