92dc91c0cf2a39e35008028c649ec51362c6b616
Jacob Bansmer <jbansmer@gmail.com>
2025-05-25 12:29:51 -0700

    changed schema of todos

--- schema.sql
+++ schema.sql
@@ -6,6 +6,6 @@ CREATE TABLE lists (
 CREATE TABLE todos (
   id serial PRIMARY KEY,
   name text NOT NULL,
-  complete boolean NOT NULL DEFAULT false,
+  completed boolean NOT NULL DEFAULT false,
   list_id integer NOT NULL REFERENCES lists (id)
 );
